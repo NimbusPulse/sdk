bdc5d9de2a2a80ff53a4177a6d36edfe79d8091a
Timm Ortloff <me@tortloff.de>
2025-10-14 00:44:02 +0200

    Rust(system-resources): Update `SystemResources` struct

--- rust/src/types/system_resources.rs
+++ rust/src/types/system_resources.rs
@@ -4,7 +4,40 @@ use serde::Deserialize;
 #[cfg_attr(test, derive(ts_rs::TS))]
 #[cfg_attr(test, ts(export, export_to = "../../javascript/lib/types/"))]
 pub struct ServerResources {
-    pub cpus: Vec<f64>,
-    pub memory: f64,
-    pub max_memory: f64,
+    pub cpus: Vec<CpuMetric>,
+    pub max_ram: u64,
+    pub ram: Vec<RamMetric>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[cfg_attr(test, derive(ts_rs::TS))]
+#[cfg_attr(test, ts(export, export_to = "../../javascript/lib/types/"))]
+pub struct CpuMetric {
+    pub metric: CpuMetricData,
+    pub values: Vec<(f64, String)>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[cfg_attr(test, derive(ts_rs::TS))]
+#[cfg_attr(test, ts(export, export_to = "../../javascript/lib/types/"))]
+pub struct CpuMetricData {
+    pub core: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[cfg_attr(test, derive(ts_rs::TS))]
+#[cfg_attr(test, ts(export, export_to = "../../javascript/lib/types/"))]
+pub struct RamMetric {
+    pub metric: RamMetricData,
+    pub values: Vec<(f64, String)>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[cfg_attr(test, derive(ts_rs::TS))]
+#[cfg_attr(test, ts(export, export_to = "../../javascript/lib/types/"))]
+pub struct RamMetricData {
+    pub instance: String,
+    pub job: String,
+    pub monitor: String,
+    pub vm_uuid: String,
 }
